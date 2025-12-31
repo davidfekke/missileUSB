b7d297b608f5ca77d6359f6958c38c1350f58137
David Fekke <david.fekke@bannerhealth.com>
2025-12-31 10:16:53 -0500

    Initial Commit

---
+++ missileUSB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  missileUSB
+//
+//  Created by David Fekke on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ missileUSB/missileUSBApp.swift
@@ -0,0 +1,17 @@
+//
+//  missileUSBApp.swift
+//  missileUSB
+//
+//  Created by David Fekke on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct missileUSBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
